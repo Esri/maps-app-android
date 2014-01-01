7b747d4593a8ea1f244f8007cdd12905ab6aae7e
Dan O'Neill <doneill@esri.com>
2013-12-31 15:03:57 -0900

    set progress dialog on address search

--- maps-app/src/com/esri/android/rt/map/MapsApp.java
+++ maps-app/src/com/esri/android/rt/map/MapsApp.java
@@ -509,9 +509,14 @@ public class MapsApp extends FragmentActivity implements
 
 		@Override
 		protected void onPreExecute() {
+      // set the message of the progress dialog
+      mProgressDialog.setMessage(getString(R.string.address_search));
+      // display the progress dialog on the UI thread
+      mProgressDialog.show();
+		  
 			// show progress dialog while geocoding address
-			mProgressDialog = ProgressDialog.show(mMapView.getContext(), "Geocoder",
-					"Searching for address ...");
+//			mProgressDialog = ProgressDialog.show(mMapView.getContext(), "Geocoder",
+//					"Searching for address ...");
 		}
 
 		// The result of geocode task is passed as a parameter to map the
@@ -700,6 +705,7 @@ public class MapsApp extends FragmentActivity implements
 		  mProgressDialog.setMessage(getString(R.string.fetching_basemaps));
 		  // display the progress dialog on the UI thread
 		  mProgressDialog.show();
+		  
 			// show progress dialog while searching basemaps
 //			mProgressDialog = ProgressDialog.show(mMapView.getContext(), "Basemaps View",
 //					"Searching Basemaps ...");
