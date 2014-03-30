32d869adbc4a72503c617dd0530f5faa998c9450
Dan O'Neill <doneill@esri.com>
2014-03-30 15:12:53 -0800

    changed access to static directly

--- maps-app/src/com/esri/android/mapsapp/tools/MeasuringTool.java
+++ maps-app/src/com/esri/android/mapsapp/tools/MeasuringTool.java
@@ -334,7 +334,7 @@ public class MeasuringTool implements Callback, OnSingleTapListener {
                 if (group.getChildAt(i).getId() == checkedId) {
                   if (mMeasureMode == MeasureType.LINEAR) {
                     if (mResult > 0) {
-                      mResult = LinearUnit.convertUnits(mResult, getLinearUnit(mCurrentLinearUnit), getLinearUnit(i));
+                      mResult = Unit.convertUnits(mResult, getLinearUnit(mCurrentLinearUnit), getLinearUnit(i));
                       mCurrentLinearUnit = i;
                       mResultView.update();
                     } else {
@@ -342,7 +342,7 @@ public class MeasuringTool implements Callback, OnSingleTapListener {
                     }
                   } else {
                     if (mResult > 0) {
-                      mResult = AreaUnit.convertUnits(mResult, getAreaUnit(mCurrentAreaUnit), getAreaUnit(i));
+                      mResult = Unit.convertUnits(mResult, getAreaUnit(mCurrentAreaUnit), getAreaUnit(i));
                       mCurrentAreaUnit = i;
                       mResultView.update();
                     } else {
