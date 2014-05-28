716711e454cc3576672900667e931037f5f4a29a
doneill <doneill@esri.com>
2014-05-28 12:54:50 -0800

    added license header

--- maps-app/src/com/esri/android/mapsapp/location/DirectionsDialogFragment.java
+++ maps-app/src/com/esri/android/mapsapp/location/DirectionsDialogFragment.java
@@ -1,3 +1,27 @@
+/* Copyright 1995-2013 Esri
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * For additional information, contact:
+ * Environmental Systems Research Institute, Inc.
+ * Attn: Contracts Dept
+ * 380 New York Street
+ * Redlands, California, USA 92373
+ *
+ * email: contracts@esri.com
+ *
+ */
+
 package com.esri.android.mapsapp.location;
 
 import java.util.List;
@@ -34,7 +58,7 @@ public class DirectionsDialogFragment extends DialogFragment {
   public interface DirectionsDialogListener {
     /**
      * Callback for when a particular direction is selected from the list.
-     * 
+     *
      * @param position Position in Routing Directions list of the selected direction.
      */
     public void onDirectionSelected(int position);
