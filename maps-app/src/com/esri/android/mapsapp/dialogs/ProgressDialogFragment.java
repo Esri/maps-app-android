43f03a30a7d4d8ebc16267471c2292da2960155b
doneill <doneill@esri.com>
2014-07-13 11:57:13 -0800

    Updated license

--- maps-app/src/com/esri/android/mapsapp/dialogs/ProgressDialogFragment.java
+++ maps-app/src/com/esri/android/mapsapp/dialogs/ProgressDialogFragment.java
@@ -1,9 +1,16 @@
-/*
- * COPYRIGHT 1995-2014 ESRI
+/* Copyright 1995-2014 Esri
  *
- * TRADE SECRETS: ESRI PROPRIETARY AND CONFIDENTIAL
- * Unpublished material - all rights reserved under the
- * Copyright Laws of the United States.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  *
  * For additional information, contact:
  * Environmental Systems Research Institute, Inc.
@@ -12,6 +19,7 @@
  * Redlands, California, USA 92373
  *
  * email: contracts@esri.com
+ *
  */
 
 package com.esri.android.mapsapp.dialogs;
