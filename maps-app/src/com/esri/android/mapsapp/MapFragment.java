8fa636e3de5b2d56fba54fe6f22aed3445a3f0cd
yash0000 <ykhandelwal@esri.com>
2014-08-08 11:23:36 -0700

    Added content desc to imageviews and removed unused imports

--- maps-app/src/com/esri/android/mapsapp/MapFragment.java
+++ maps-app/src/com/esri/android/mapsapp/MapFragment.java
@@ -55,7 +55,6 @@ import android.view.inputmethod.InputMethodManager;
 import android.widget.FrameLayout;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
-import android.widget.LinearLayout.LayoutParams;
 import android.widget.SearchView;
 import android.widget.SearchView.OnQueryTextListener;
 import android.widget.TextView;
