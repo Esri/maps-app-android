405475d0c0dc01ae42a417711a544fc30908fb65
doneill <doneill@esri.com>
2014-08-21 12:36:25 -0800

    Updated comments

--- maps-app/src/com/esri/android/mapsapp/MapsAppActivity.java
+++ maps-app/src/com/esri/android/mapsapp/MapsAppActivity.java
@@ -146,14 +146,8 @@ public class MapsAppActivity extends Activity {
 		mDrawerToggle = new ActionBarDrawerToggle(this, /* host Activity */
 		mDrawerLayout, /* DrawerLayout object */
 		R.drawable.ic_drawer, /* nav drawer image to replace 'Up' caret */
-		R.string.navigation_drawer_open, /*
-										 * "open drawer" description for
-										 * accessibility
-										 */
-		R.string.navigation_drawer_close /*
-										 * "close drawer" description for
-										 * accessibility
-										 */
+		R.string.navigation_drawer_open, /* "open drawer" description for accessibility */
+		R.string.navigation_drawer_close /* "close drawer" description for accessibility */
 		) {
 			@Override
 			public void onDrawerClosed(View drawerView) {
