26904aa89d4b7e7c8fa2bfad7b2f5eb8fe5d26d0
Dan O'Neill <doneill@esri.com>
2016-03-06 20:15:23 -0900

    initial implementation of FAB

--- maps-app/src/main/java/com/esri/android/mapsapp/MapsAppActivity.java
+++ maps-app/src/main/java/com/esri/android/mapsapp/MapsAppActivity.java
@@ -25,7 +25,6 @@
 package com.esri.android.mapsapp;
 
 import android.app.ActionBar;
-import android.app.Activity;
 import android.app.Fragment;
 import android.app.FragmentManager;
 import android.app.FragmentTransaction;
@@ -35,6 +34,7 @@ import android.os.Bundle;
 import android.support.v4.app.ActionBarDrawerToggle;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
@@ -59,7 +59,7 @@ import butterknife.InjectView;
 /**
  * Entry point into the Maps App.
  */
-public class MapsAppActivity extends Activity {
+public class MapsAppActivity extends AppCompatActivity {
 
 	public static DrawerLayout mDrawerLayout;
 	
