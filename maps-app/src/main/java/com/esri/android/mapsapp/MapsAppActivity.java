2548ba6bce9e395565f8b947fb5358ed2eba0145
Dan O'Neill <doneill@esri.com>
2016-03-07 16:12:34 -0900

    implemented location from fab, removed from action bar

--- maps-app/src/main/java/com/esri/android/mapsapp/MapsAppActivity.java
+++ maps-app/src/main/java/com/esri/android/mapsapp/MapsAppActivity.java
@@ -34,7 +34,6 @@ import android.support.v4.app.ActionBarDrawerToggle;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
@@ -116,10 +115,6 @@ public class MapsAppActivity extends AppCompatActivity {
 			handled = super.onOptionsItemSelected(item);
 		}
 		return handled;
-	}
-
-	public void onClick(View view){
-		Log.d("***", "onClick called with: " + "view = [" + view + "]");
 	}
 
 	/**
